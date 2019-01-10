9f26c4a727952cb21489cf0421c79d189abdedb9
haleybarlar <hbarlar1@gmail.com>
2019-01-10 13:31:39 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>This is the Navbar!</h1>
+      </div>
+    )
+  }
+}
+
+export default Navbar
