ecae444858d872fda0100a177f6bc9cf3124f91c
haleybarlar <hbarlar1@gmail.com>
2019-01-10 14:31:58 -0500

    home page images added

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,7 +6,13 @@ class Work extends Component {
     return (
       <div>
         <h1>Development</h1>
+        <img src={'./mindful.png'} alt=""/>
+        <img src={'./yogaCycle.png'} alt=""/>
+        <img src={'./DH.png'} alt=""/>
         <h1>Fibers</h1>
+        <img src={'./group.jpg'} alt=""/>
+        <img src={'./beaded.JPG'} alt=""/>
+        <img src={'./molded.JPG'} alt=""/>
       </div>
     )
   }
