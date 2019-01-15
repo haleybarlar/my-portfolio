b90dda7dd035416d4b35e043840651ada81c1273
haleybarlar <hbarlar1@gmail.com>
2019-01-14 20:09:44 -0500

    for heroku

--- src/projects_data.js
+++ src/projects_data.js
@@ -2,7 +2,7 @@ const projects = [
   {
     id: 1,
     name: 'Mindful Todo',
-    description: "TLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "TLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     languages: "Rails, React, Redux, Semantic UI, CSS",
     image: ['./mindful.png'],
     type: 'development',
