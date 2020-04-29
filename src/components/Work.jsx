e92481f8e45c6c0f853f3682412032847519c010
Haley Barlar <haley.barlar@blacklane.com>
2020-04-29 13:38:31 -0400

    whoops

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -9,7 +9,7 @@ export default function Work(props) {
   }, []);
 
   const fetchAll = async () => {
-    const key = "keyWviNJiEc3mUgXV";
+    const key = process.env.REACT_APP_AIRTABLE_API_KEY;
     const url = "https://api.airtable.com/v0/appa3x0Ddhl3fU1h9/Table%201%20/";
 
     const resp = await fetch(url, {
