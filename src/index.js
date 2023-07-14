d2eb9b0941659da4d2623bc2472d7eeb481cd17c
Haley Barlar <haley@vertis.ai>
2023-07-14 18:08:50 -0400

    ?

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { App } from "./App.tsx";
 import * as serviceWorker from "./serviceWorker";
-require("dotenv").config();
 
 ReactDOM.render(
   <BrowserRouter>
